1b22f080056a1e99a2bc18c85810ca1c046443f0
Rose Mullan <rfmullan@gmail.com>
2022-11-08 10:37:35 +0000

    add reliance on environment variables

--- db/index.js
+++ db/index.js
@@ -1,7 +1,21 @@
 const { Pool } = require('pg');
+const ENV = process.env.NODE_ENV || "development";
+require("dotenv").config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
-var conString = "postgres://uswxctwx:EfppvI1HJmQq8GFxdtRmbYvogl3glesv@surus.db.elephantsql.com/uswxctwx" //Can be found in the Details page
-var pool = new Pool({connectionString: conString});
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+      }
+    : {};
+
+
+const pool = new Pool(config);
 pool.connect(function(err) {
   if (err) {
     return console.error('could not connect to postgres', err);
