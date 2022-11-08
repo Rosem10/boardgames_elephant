c8f9206d54adf9dd64d419d088e5034d78d85e2c
Rose Mullan <rfmullan@gmail.com>
2022-11-08 09:51:07 +0000

    refactor pg requirement

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
-const pg = require('pg');
+const {Pool} = require('pg');
 
 var conString = "postgres://uswxctwx:EfppvI1HJmQq8GFxdtRmbYvogl3glesv@surus.db.elephantsql.com/uswxctwx" //Can be found in the Details page
-var pool = new pg.Pool({connectionString: conString});
+var pool = new Pool({connectionString: conString});
 pool.connect(function(err) {
   if (err) {
     return console.error('could not connect to postgres', err);
